2697e91caa29a62916d34ead816c396180db93a6
growingpsy <psy915@swu.ac.kr>
2025-04-20 01:16:52 +0900

    [week3] pub_date 필드를 auto_now_add=True로 수정

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'제목: {self.question_text}, 날짜: {self.pub_date}'
