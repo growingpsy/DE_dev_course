972ea20741aad9bccdc726e31ac42c5e51bc2ed5
growingpsy <psy915@swu.ac.kr>
2025-04-19 22:15:03 +0900

    [week3] 모델을 admin에 등록, __str__정의

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,6 +9,8 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return f'제목: {self.question_text}, 날짜: {self.pub_date}'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
