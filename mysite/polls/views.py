415c11b641ce92b4547dc28b75005debe2ce8c06
growingpsy <psy915@swu.ac.kr>
2025-04-21 00:27:28 +0900

    [week3] vote로직을 변경하여 동시성 에러 방지

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.db.models import F
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -25,8 +26,11 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {'question': question, 'error_message': '선택이 없습니다.'})
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': f"선택이 없습니다. id={request.POST['choice']}"})
     else:
-        selected_choice.votes += 1
+        # A서버에서도 Votes = 1
+        # B서버에서도 Votes = 1 
+        # selected_choice.votes +=  1
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:index'))
