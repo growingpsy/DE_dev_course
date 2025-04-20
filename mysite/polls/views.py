79087ea5bbefd2318323d8f68611cfa37349945a
growingpsy <psy915@swu.ac.kr>
2025-04-20 22:33:16 +0900

    [week3] 404 에러 처리하기

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
 from .models import *
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -12,5 +13,11 @@ def some_url(request):
     return HttpResponse("Some_url 구현하기")
 
 def detail(request, question_id):
-    question = Question.objects.get(pk=question_id)
+    """
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    """
+    question = get_object_or_404(Question, pk=question_id)  
     return render(request, 'polls/detail.html', {'question': question})
