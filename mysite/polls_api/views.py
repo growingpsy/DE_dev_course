d98cef962f282cbcb4da1d9349ea7db3f992575e
growingpsy <psy915@swu.ac.kr>
2025-04-21 03:21:59 +0900

    [week3] 제네릭 뷰 클래스로 리팩토링

--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,32 +1,11 @@
 from polls.models import Question
 from polls_api.serializers import QuestionSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
-class QuestionList(mixins.ListModelMixin,
-                    mixins.CreateModelMixin,
-                    generics.GenericAPIView):
+class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class QuestionDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
