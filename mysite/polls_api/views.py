f8587fc955c7a3d6183885877f430f7cd6109b99
growingpsy <psy915@swu.ac.kr>
2025-05-07 02:56:28 +0900

    [week3] Serialize를 이용한 User생성

--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,5 +1,5 @@
 from polls.models import Question
-from polls_api.serializers import QuestionSerializer, UserSerializer
+from polls_api.serializers import QuestionSerializer, UserSerializer, RegisterSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 
@@ -18,3 +18,6 @@ class UserList(generics.ListAPIView): # User 목록 조회
 class UserDetail(generics.RetrieveAPIView): # 단일 User 상세 조회
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class RegisterUser(generics.CreateAPIView): # User 등록
+    serializer_class = RegisterSerializer
