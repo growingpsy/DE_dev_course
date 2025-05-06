43ced1e8e5992cd9873ec7261c3e431f118703cf
growingpsy <psy915@swu.ac.kr>
2025-05-07 00:40:10 +0900

    [week3] User 목록 조회 및 단일 조회 API 구현

--- mysite/polls_api/views.py
+++ mysite/polls_api/views.py
@@ -1,6 +1,7 @@
 from polls.models import Question
-from polls_api.serializers import QuestionSerializer
+from polls_api.serializers import QuestionSerializer, UserSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
@@ -9,3 +10,11 @@ class QuestionList(generics.ListCreateAPIView):
 class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+
+class UserList(generics.ListAPIView): # User 목록 조회
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView): # 단일 User 상세 조회
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
